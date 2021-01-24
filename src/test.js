069314bad1bcb062bfaa61f0a1fb1d196b56e8d3
Jacob Tepperman <jtepp@icloud.com>
2021-01-23 23:53:40 -0500

    uh i think its working

---
+++ src/test.js
@@ -0,0 +1,12 @@
+
+exports.handler = async (event, context) => {
+
+        return ({
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+                "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+            },
+            body: "test"})
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
